b80efe03ac20132a564aef5a72cb55c0545196c0
Emili Sophia Cordeiro <emilisophiassss@gmail.com>
2024-11-21 08:41:41 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 let baralho = []
+
 while (true) {
     let menu = `Baralho de cartas:
 Quantidade de cartas: ${baralho.length}
@@ -6,23 +7,29 @@ Escolha uma opção:
 1. Adicionar uma carta
 2. Puxar uma carta
 3. Sair`
+
     let escolha = prompt(menu)
+
     if (escolha === "1") {
         let nomeCarta = prompt("Digite o nome da carta:")
-        if (nomeCarta) {
+
+        if (nomeCarta && nomeCarta.trim() !== "") {
             baralho.push(nomeCarta)
-            alert(`${nomeCarta} foi adicionada ao topo do baralho.`) } 
-        else {
-            alert("Nome da carta inválido! Tente novamente.") }
+            alert(`${nomeCarta} foi adicionada ao topo do baralho.`)
+        } else {
+            alert("Nome da carta inválido! Tente novamente.")
+        }
     } else if (escolha === "2") {
         if (baralho.length === 0) {
-            alert("O baralho está vazio.")} 
-        else {
+            alert("O baralho está vazio.")
+        } else {
             let cartaPuxada = baralho.pop()
-            alert(`Você puxou a carta: ${cartaPuxada}`) }
+            alert(`Você puxou a carta: ${cartaPuxada}`)
+        }
     } else if (escolha === "3") {
         alert("Encerrando programa. Até mais!")
-        break} 
-    else {
-        alert("Opção inválida! Tente novamente.") }
+        break
+    } else {
+        alert("Opção inválida! Tente novamente.")
+    }
 }
